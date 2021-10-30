756e187b575b61847a75c7aae0563816068a82df
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-10-30 14:27:04 +0530

    Update not.vhd

--- not.vhd
+++ not.vhd
@@ -1,3 +1,12 @@
 # vhdl_codes
   ****NOT_GATE circuit design****
-  
+ library IEEE;
+use IEEE.std_logic_1164.all;
+entity not_gate is
+    port(A : in std_logic;          
+         Z : out std_logic); 
+end not_gate;
+ architecture basic_gate of not_gate is
+begin
+    Y <= not(A) ;
+end basic_gate;
