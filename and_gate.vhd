74f77d0427d07549e70c6e3fb7a0885668fad129
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-01 23:16:05 +0530

    Update and_gate.vhd

--- and_gate.vhd
+++ and_gate.vhd
@@ -1,1 +1,12 @@
 ****AND_Gate circuit design****
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity and_gate is
+    port(A : in std_logic;
+         B : in std_logic;
+         Z : out std_logic); 
+end and_gate;
+ architecture basic_gate of and_gate is
+begin
+    Z <= A and B ;
+end basic_gate;
