160b56e22a75c7ccc3d74e7eeb7299ff07af1263
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-04 11:18:15 +0530

    Update nor_gate.vhd

--- nor_gate.vhd
+++ nor_gate.vhd
@@ -1,1 +1,12 @@
-
+****NOR_Gate circuit design
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity nor_gate is
+    port(A : in std_logic;
+         B : in std_logic;
+         Z : out std_logic); 
+end nor_gate;
+ architecture basic_gate of nor_gate is
+begin
+    Z <= A nor B ;
+end basic_gate;
