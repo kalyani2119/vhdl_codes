67f9d21015213a8c97ebc8bd28e8eaddb406afba
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-04 11:16:44 +0530

    Update nand_gate.vhd

--- nand_gate.vhd
+++ nand_gate.vhd
@@ -1,1 +1,12 @@
-
+****NAND_Gate circuit design****
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity nand_gate is
+    port(A : in std_logic;
+         B : in std_logic;
+         Z : out std_logic); 
+end nand_gate;
+ architecture basic_gate of nand_gate is
+begin
+    Z <= A nand B ;
+end basic_gate;
