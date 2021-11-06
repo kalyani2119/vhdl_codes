135be295fde80ba746d884e49a6e03af481a15be
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-06 23:00:54 +0530

    Update xor_gate.vhd

--- xor_gate.vhd
+++ xor_gate.vhd
@@ -1,1 +1,12 @@
-
+****XOR_Gate circuit design****
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity xor_gate is
+    port(A : in std_logic;
+         B : in std_logic;
+         Z : out std_logic); 
+end xor_gate;
+ architecture basic_gate of xor_gate is
+begin
+    Z <= A xor B ;
+end basic_gate;
